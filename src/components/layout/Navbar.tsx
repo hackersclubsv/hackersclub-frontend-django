e0d94b7206bfffc9e894f20711f89dc5084e80f4
Yang Jiang <jiangyang.jnu@gmail.com>
2023-07-16 21:48:53 -0700

    setup Login and Auth

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,17 @@
+import { useAuth } from "../../hooks/useAuth";
 
 const Navbar = ({isLogin} : {isLogin: Boolean}) => {
   
+  const {user, logout} = useAuth();
+  console.log("user in nav bar", user);
   const loginComponent = (
-    isLogin ? (
+    user !== null ? (
       <ul className="navbar-nav mb-2 mb-lg-0">
       <li className="nav-item">
-      <a className="nav-link" href="/userprofile">Username</a>
+      <a className="nav-link" href="/userprofile">{user.name}</a>
       </li>
       <li className="nav-item">
-      <button className="nav-link">Logout</button>
+      <button className="nav-link" onClick={logout}>Logout</button>
       </li>
     </ul> 
     ) : 
@@ -24,7 +27,7 @@ const Navbar = ({isLogin} : {isLogin: Boolean}) => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">SV Hackers Club</a>
+        <h1><a className="navbar-brand" href="/">SV Hackers Club</a></h1>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,48 +1,80 @@
-import { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+
 
 type loginProps = {
-  isLogin: Boolean, 
-  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>
-}
+  isLogin: Boolean;
+  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-const Login = ({isLogin, setIsLogin} : loginProps) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = ({ isLogin, setIsLogin }: loginProps) => {
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      setEmail('');
+      setPassword('');
+      setIsLogin(true);
+     
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGuestLogin = () => {
+    login({
+      id: '1',
+      name: 'John Doe',
+      email: 'john.doe@gmail.com'
+    });    
+  };
+  console.log(isLogin, " in login ts");
+
 
   return (
-    <div className='container'>
-      <form onSubmit={e => {e.preventDefault(); setIsLogin(true)}}>
+    <div className="container">
+      <div className='mb-3'>
+        <button className='btn btn-primary' onClick={handleGuestLogin}>Guest Login</button>
+      </div>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+          <label htmlFor="email" className="form-label">
             Email address
           </label>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            onChange={e => setEmail(e.target.value)}
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
           <input
             type="password"
             className="form-control"
-            id="exampleInputPassword1"
-            onChange={e => setPassword(e.target.value)}
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
           />
         </div>
         <button type="submit" className="btn btn-primary">
-          Submit
+          Sign In
         </button>
-        <div className='contianer'>
-          <a href="/register">Not on SV Hackers Club yet? Sign up!</a>
+        <div className="contianer">
+          <span>
+            Not on SV Hackers Club yet? <br />
+            <a href="/register">Sign up!</a>
+          </span>
         </div>
       </form>
     </div>
