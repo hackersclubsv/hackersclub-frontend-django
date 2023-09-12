f2b95182084bb427832d88634684f73a5e1b4cd5
hongji <shihongji21@gmail.com>
2023-09-12 14:11:08 -0700

    Resize the img if it's too large

--- src/components/common/WikiContent.js
+++ src/components/common/WikiContent.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { Box, Typography, Link, Alert } from '@mui/material';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import React, { useEffect, useMemo, useState } from "react";
+import { Alert, Box, Link, Typography } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-import getting_started from '../../assets/wiki/getting_started.md';
-import campus_useful_links from '../../assets/wiki/campus_useful_links.md';
-import learning_resources from '../../assets/wiki/learning_resources.md';
-import useful_jobHunting_sites from '../../assets/wiki/useful_jobHunting_sites.md';
-import SV_hackers_club from '../../assets/wiki/SV_hackers_club.md';
-import frequently_asked_question from '../../assets/wiki/frequently_asked_question.md';
+import getting_started from "../../assets/wiki/getting_started.md";
+import campus_useful_links from "../../assets/wiki/campus_useful_links.md";
+import learning_resources from "../../assets/wiki/learning_resources.md";
+import useful_jobHunting_sites from "../../assets/wiki/useful_jobHunting_sites.md";
+import SV_hackers_club from "../../assets/wiki/SV_hackers_club.md";
+import frequently_asked_question from "../../assets/wiki/frequently_asked_question.md";
 
 const WikiContent = ({ content: fileName }) => {
-  const [content, setContent] = useState('');
-  const contents = useMemo(() => ({
-    getting_started,
-    campus_useful_links,
-    learning_resources,
-    useful_jobHunting_sites,
-    SV_hackers_club,
-    frequently_asked_question,
-  }), []);
+  const [content, setContent] = useState("");
+  const contents = useMemo(
+    () => ({
+      getting_started,
+      campus_useful_links,
+      learning_resources,
+      useful_jobHunting_sites,
+      SV_hackers_club,
+      frequently_asked_question,
+    }),
+    [],
+  );
 
   useEffect(() => {
     if (!fileName) {
@@ -37,7 +40,15 @@ const WikiContent = ({ content: fileName }) => {
   return (
     <Box>
       {fileName ? (
-        <ReactMarkdown remarkPlugins={[remarkGfm]} children={content} />
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          children={content}
+          components={{
+            img: ({ node, ...props }) => (
+              <img {...props} style={{ maxWidth: "100%" }} />
+            ),
+          }}
+        />
       ) : (
         <>
           <Typography variant="h5">Welcome to San Jose Campus!</Typography>
@@ -46,9 +57,12 @@ const WikiContent = ({ content: fileName }) => {
       )}
       <Alert severity="info">
         <Typography>
-          Have questions? <Link href="/community">Discuss with others</Link> or <Link href='/about'>Contact Us!</Link> <br />
-          Interested in contributing to this post?   
-          <Link href="https://github.com/hackersclubsv/hackersclub-frontend-django/tree/main/src/assets/wiki">Create a pull request on our GitHub!</Link>
+          Have questions? <Link href="/community">Discuss with others</Link> or{" "}
+          <Link href="/about">Contact Us!</Link> <br />
+          Interested in contributing to this post?
+          <Link href="https://github.com/hackersclubsv/hackersclub-frontend-django/tree/main/src/assets/wiki">
+            Create a pull request on our GitHub!
+          </Link>
         </Typography>
       </Alert>
     </Box>
