7ab8c0d222a981f6d1d8584cc6aed1e713d26efe
Yang Jiang <jiangyang.jnu@gmail.com>
2023-09-11 19:47:03 -0700

    Update About page layout

---
+++ src/assets/page_contents/about.js
@@ -0,0 +1,71 @@
+export const headMessage = "Welcome to SV Hackers Club, a Student Interest Group ratified and recognized by Northeastern University at the Silicon Valley campus! We are a dynamic and passionate community of like-minded individuals who share a common interest in software development. Whether you're an enthusiastic beginner or a seasoned expert, our club offers a platform to learn, collaborate, and thrive."
+export const logoIntro = {
+  img: "logoIntro.png"
+}
+
+export const ourGoal = [
+  {
+    title: "A project-based mutual learning environment",
+    description: "Elevate students’ software development and programming skills, and gain proficiency in diverse tech stacks."
+},
+{
+    title: "Bridging the gap between students and alumni",
+    description: "as well as those with co-op experience. Fostering lasting and meaningful connections and offering support and advice on job-hunting."
+}
+]
+
+export const benefits = [
+  {
+    title: "Enhanced Event Opportunities",
+    description: "Access to a wider range of events with dedicated funding, providing enriching experiences and opportunities for personal and professional growth."
+  },
+  {
+    title: "Expanded Networking Opportunities",
+    description: " Connect and engage with fellow students and accomplished alumni, fostering valuable relationships that can lead to mentorship and future collaborations.​"
+  },
+  {
+    title: "Resume-Boosting Project Experience",
+    description: "Gain hands-on experience by participating in impactful projects, adding a significant edge to your resume and demonstrating practical skills to potential employers.​"
+  },
+  {
+    title: "More Exposure to Recruiters",
+    description: "Leverage unique opportunities to directly interact with recruiters and industry professionals. Showcase your talents and accomplishments to potential employers through exclusive networking events, recruitment drives, and tailored interactions. ​"
+  },
+  {
+    title: "Access to Exclusive Resources",
+    description: "Gain access to a curated collection of educational materials, e.g., funds for Udemy courses, Leetcode resources, interview practices, etc.​"
+  },
+]
+export const members = [
+  {
+    name: "Yang Jiang",
+    position: "President",
+    role: "PM, Frontend",
+    contact: "jiang.yang1@northeastern.edu"
+  },
+  {
+    name: "Hongji Shi",
+    position: "Vice President",
+    role: "Full Stack",
+  },
+  {
+    name: "Raj Sudharshan Vidyalatha Natarajan",
+    position: "Vice President",
+    role: "Backend",
+  },
+  {
+    name: "Venni Yu",
+    position: "Member",
+    role: "Frontend",
+  },
+  {
+    name: "Fiona Xiong",
+    position: "Member",
+    role: "Frontend",
+  },
+  {
+    name: "Fia Yin",
+    position: "Member",
+    role: "Frontend",
+  }
+]
