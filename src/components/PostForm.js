c0da65010b5fbea81fa078a4887c6a8d7d65b9a6
hongji <shihongji21@gmail.com>
2023-09-18 12:22:02 -0700

    Create new Post imp

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -8,15 +8,14 @@ import {
   Typography,
 } from "@mui/material";
 import Editor from "../components/Editor.js";
+import axios from "../api/axios.js";
 
 const PostForm = ({ initialPost = {}, onSave }) => {
   // Hooks for form fields, initial values are from props or empty strings.
   // Assume that initialPost = { author: '', title: '', url: '', text: '', categoryId: '' }
-  const [author, setAuthor] = useState(initialPost.author || "");
   const [title, setTitle] = useState(initialPost.title || "");
-  const [url, setUrl] = useState(initialPost.url || "");
-  const [text, setText] = useState(initialPost.text || "");
   const [categoryId, setCategoryId] = useState(initialPost.categoryId || "");
+  const [tags, setTags] = useState(initialPost.input_tags || []);
   const [value, setValue] = useState(initialPost.text || "**Hello world!!!**");
 
   // Handler for the Submit button
@@ -24,14 +23,32 @@ const PostForm = ({ initialPost = {}, onSave }) => {
     e.preventDefault();
 
     const postToSave = {
-      author,
       title,
-      url,
-      text,
-      categoryId,
+      content: value, // Assuming value here is going to be our content for the post
+      category_name: categoryId,
+      input_tags: tags,
     };
+    try {
+      // Get the access token from local storage
+      const accessToken = localStorage.getItem("accessToken");
+      const response = await axios.post("/posts/", postToSave, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-    onSave(postToSave); // This would be a function provided by the parent component where you would handle the API call to create/edit post
+      if (response.status === 200 || response.status === 201) {
+        // This will be triggered if the post was successful,
+        // you can add any process you want to happen after a successful post like clearing the form
+        console.log("Post was successful", response.data);
+        setTitle("");
+        setCategoryId("");
+        setTags([]);
+        setValue("**Hello world!!!**");
+      }
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -41,7 +58,6 @@ const PostForm = ({ initialPost = {}, onSave }) => {
       </Typography>
       <form onSubmit={handleSubmit}>
         <Grid container spacing={2}>
-
           <Grid item xs={12}>
             <TextField
               fullWidth
@@ -65,8 +81,9 @@ const PostForm = ({ initialPost = {}, onSave }) => {
               fullWidth
               label="tag"
               variant="outlined"
-              value={categoryId}
-              onChange={(e) => setCategoryId(e.target.value)}
+              value={tags.join(",")}
+              onChange={(e) => setTags(e.target.value.split(","))}
+              helperText="Enter tags comma separated."
             />
           </Grid>
           <Grid item xs={12}>
