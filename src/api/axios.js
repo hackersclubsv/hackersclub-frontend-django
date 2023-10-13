e9051b4b51c16682d6472b21110b6fa7ae63f03a
hongji <shihongji21@gmail.com>
2023-10-13 11:07:18 -0700

    Fix: Add declaratio to env variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export default axios.create({
   // (Delete when finish setting up this in CI/CD pipelines -> baseURL: 'https://api.svhackersclub.com/api/',
   // process.env is a global object in Node.js, and it is used to access application environment variables.
-  baseURL: process.env.baseURL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
