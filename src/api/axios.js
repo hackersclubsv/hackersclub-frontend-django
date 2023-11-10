21854f2da6ed43b868fd0e84266d1f5ab57bdfb0
hongji <shihongji21@gmail.com>
2023-11-10 12:04:28 -0800

    env.development added, based on NODE_ENV

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 export default axios.create({
   // (Delete when finish setting up this in CI/CD pipelines -> baseURL: 'https://api.svhackersclub.com/api/',
   // process.env is a global object in Node.js, and it is used to access application environment variables.
+  // Must prefix with REACT_APP_ to use in REACT 
+  // Must restart server to see changes
   baseURL: process.env.REACT_APP_BASE_URL,
 });
