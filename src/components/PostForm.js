7de653a11aca3bd659c3759a7ed3faf60e057d9e
hongji <shihongji21@gmail.com>
2023-12-20 10:34:36 -0800

    Update PostList and PostForm to make category field only selectable

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   Box,
   Button,
   Container,
+  FormControl,
   Grid,
+  InputLabel,
+  MenuItem,
+  Select,
   TextField,
   Typography,
 } from "@mui/material";
@@ -19,6 +23,12 @@ const PostForm = ({ initialPost = {}, onSave }) => {
   const [tags, setTags] = useState(initialPost.input_tags || []);
   const [value, setValue] = useState(initialPost.text || "**Hello world!!!**");
   const navigate = useNavigate();
+  const categories = [
+    { id: 1, name: "Announcements" },
+    { id: 5, name: "Campus Life" },
+    { id: 4, name: "Job Hunting" },
+    { id: 6, name: "Tech Dojo" },
+  ];
 
   // Handler for the Submit button
   const handleSubmit = async (e) => {
@@ -27,7 +37,7 @@ const PostForm = ({ initialPost = {}, onSave }) => {
     const postToSave = {
       title,
       content: value, // Assuming value here is going to be our content for the post
-      category_name: categoryId,
+      category_name: categories.find((category) => category.id === categoryId).name,
       input_tags: tags,
     };
     try {
@@ -66,14 +76,21 @@ const PostForm = ({ initialPost = {}, onSave }) => {
             />
           </Grid>
           <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="Category"
-              variant="outlined"
-              value={categoryId}
-              onChange={(e) => setCategoryId(e.target.value)}
-              helperText="Choose a category. Can't leave empty."
-            />
+            <FormControl fullWidth>
+              <InputLabel id="category-select-label">Category</InputLabel>
+              <Select
+                labelId="category-select-label"
+                label="Category"
+                value={categoryId}
+                onChange={(e) => setCategoryId(e.target.value)}
+              >
+                {categories.map((category) => (
+                  <MenuItem key={category.id} value={category.id}>
+                    {category.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
           <Grid item xs={6}>
             <TextField
